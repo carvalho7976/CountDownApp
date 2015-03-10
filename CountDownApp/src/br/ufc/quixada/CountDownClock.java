562ea538fe5e502b0b9265da5e5ce4f959bce91e
rogerio <rogerio_carvalho15@hotmail.com>
2015-03-09 21:26:34 -0300

    classes escritas novamente, resolver bug

--- CountDownApp/src/br/ufc/quixada/CountDownClock.java
+++ CountDownApp/src/br/ufc/quixada/CountDownClock.java
@@ -1,5 +1,37 @@
 package br.ufc.quixada;
 
-public class CountDownClock {
+public class CountDownClock extends Thread implements ITimeMonitor {
+	private int t;
+	
+	public CountDownClock(int start) {
+		this.t = start;
+	}
+	public void run(){
+		boolean aborted = false;
+		for (; t >= 0; t--){
+			System.out.println("T minus " + t);
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
+				aborted = true;
+			}
+			if (Thread.interrupted())
+				aborted = true;
+				if (aborted){
+					System.out.println("Stopping the clock!");
+					break;
+				}
+		}
+	}
+	public synchronized void abortCountDown()	{
+		Thread[] threads = new Thread[Thread.activeCount()];
+		Thread.enumerate(threads);
+		for(Thread t : threads)
+			t.interrupt();
+	}
 
+	@Override
+	public int getTime() {
+		return t;
+	}
 }
